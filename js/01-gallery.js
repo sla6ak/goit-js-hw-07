695184eb8168580ecbece2dba2fa8189726e1bf5
sla6ak <slabakxaker@gmail.com>
2021-12-07 14:09:58 +0200

    ES6

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,63 +2,118 @@ import { galleryItems } from './gallery-items.js';
 // import * as basicLightbox from 'basiclightbox';
 // Change code below this line
 
-console.log(galleryItems);
+console.log( galleryItems );
+
+// *************************дополнительное задание использовать классы-ES6 в решении**********************************
+
+// 1) создадим клас изображений
+// в который покладем все свойства из массива
+
+class Gallerey1 {
+    constructor (preview, original, description) {
+        this.preview = preview
+        this.original = original
+        this.description = description
+   }
+}
 
 // получим родителя для нашей галереии
 const gallerey = document.querySelector( '.gallery' )
 
-// напмшем функцию для динамически создаваемой галереии в html
-const html = ( gallery ) => {
+
+// функция динамической разметки передаем в нее любой  массив изображений и рендерим разметку
+const html = (gallery) => {
     return gallery.map( ( img ) => {
-            // создадим обертку для изображения "див"
-            let boxEl = document.createElement( "div" )
-            boxEl.classList.add( 'gallery__item' )
-            // обернем изображение в ссылку
-            let link = document.createElement( "a" )
-            link.classList.add( 'gallery__link' )
-            link.href = img.original
-            // создадим тег изображения с объязательными полями размеров и подпись
-            let imgEl = document.createElement( "img" )
-            imgEl.src = img.preview
-            imgEl.classList.add('gallery__image')
-            imgEl.width = '509'
-            imgEl.height = '340'
-            imgEl.alt = img.description
-            link.append(imgEl) 
-            boxEl.append( link )
-            // вернем уже набраные элементы в массив
-            return boxEl
-    });
+        const imgEl = new Gallerey1( img.preview, img.original, img.description )
+        const htmlEl = `<div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="${imgEl.preview}"
+                alt="${imgEl.description}"
+                data-source="${imgEl.original}"
+                />
+            </a>
+            </div>`;
+            return htmlEl
+    })
 }
-// распылим получившуюся верстку в dom за один раз
-gallerey.append(...html(galleryItems))
+// повесим нашу разметку на сайт
+gallerey.insertAdjacentHTML("beforeend", html(galleryItems).join(""))
 
-// мы имеем галерею - создадим функционал прослушивая клики по каждому img
-gallerey.addEventListener( 'click', onImjClick )
+gallerey.addEventListener( "click", onOriginalClick )
 
-// функция обрабатывающая событие клика по изображению
-function onImjClick ( e ) {
+function onOriginalClick ( e ) {
     e.preventDefault();
-
     if ( e.target.nodeName !== 'IMG' ) {
         return
     }
-
-    // для наглядности вынесем елемент клика в переменную
-    let clickImg = e.target
-
-    // подготовим переменную для изображения модального окна
-    let srcEl = "";
-
-    // найдем по какому изображению в массиве был клик и вытянем оригинальное фото из массива
-    galleryItems.forEach( ( img ) => {
-        if ( clickImg.src === img.preview ) {
-            srcEl = img.original
-        }  
-    })
-    
-    // эта часть кода написанна не мной а взята из документации библиотеки
+console.log(e.target.dataset.source)
     basicLightbox.create(`
-    <img width="1400" height="900" src="${srcEl}">
-    `).show()
+    <img width="1400" height="900" src="${e.target.dataset.source}">
+     `).show()
 }
+
+
+// *********************************предыдущее решение************************************************
+
+
+// // получим родителя для нашей галереии
+// const gallerey = document.querySelector( '.gallery' )
+
+// // напмшем функцию для динамически создаваемой галереии в html
+// const html = ( gallery ) => {
+//     return gallery.map( ( img ) => {
+//             // создадим обертку для изображения "див"
+//             let boxEl = document.createElement( "div" )
+//             boxEl.classList.add( 'gallery__item' )
+//             // обернем изображение в ссылку
+//             let link = document.createElement( "a" )
+//             link.classList.add( 'gallery__link' )
+//             link.href = img.original
+//             // создадим тег изображения с объязательными полями размеров и подпись
+//             let imgEl = document.createElement( "img" )
+//             imgEl.src = img.preview
+//             imgEl.classList.add('gallery__image')
+//             imgEl.width = '509'
+//             imgEl.height = '340'
+//             imgEl.alt = img.description
+//             link.append(imgEl) 
+//             boxEl.append( link )
+//             // вернем уже набраные элементы в массив
+//             return boxEl
+//     });
+// }
+// // распылим получившуюся верстку в dom за один раз
+// gallerey.append(...html(galleryItems))
+
+// // мы имеем галерею - создадим функционал прослушивая клики по каждому img
+// gallerey.addEventListener( 'click', onImjClick )
+
+// // функция обрабатывающая событие клика по изображению
+// function onImjClick ( e ) {
+//     e.preventDefault();
+
+//     if ( e.target.nodeName !== 'IMG' ) {
+//         return
+//     }
+
+//     // для наглядности вынесем елемент клика в переменную
+//     let clickImg = e.target
+
+//     // подготовим переменную для изображения модального окна
+//     let srcEl = "";
+
+//     // найдем по какому изображению в массиве был клик и вытянем оригинальное фото из массива
+//     galleryItems.forEach( ( img ) => {
+//         if ( clickImg.src === img.preview ) {
+//             srcEl = img.original
+//         }  
+//     })
+    
+//     // эта часть кода написанна не мной а взята из документации библиотеки
+//     basicLightbox.create(`
+//     <img width="1400" height="900" src="${srcEl}">
+//     `).show()
+// }
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,33 +1,76 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+console.log( galleryItems );
 
-const gallerey = document.querySelector( '.gallery' )
+// *****************************дополнительное задание использовать классы ES6***************************************
 
-// уже знакомый метод верстки через шаблонную функцию
-const imgEl = (galleryItems) => {
-    return galleryItems.map( ( img ) => {
-        let linkEl = document.createElement( "a" )
-        linkEl.classList.add( "gallery__item" )
-        linkEl.href = img.original
-        let imgEl = document.createElement( "img" )
-        imgEl.src = img.preview
-        imgEl.alt = img.description
-        imgEl.title= img.description
-        imgEl.classList.add( "gallery__image" )
-        linkEl.append(imgEl)
-        return linkEl
-    })
+class Gallerey1 {
+    constructor (preview, original, description) {
+        this.preview = preview
+        this.original = original
+        this.description = description
+   }
 }
 
-gallerey.append( ...imgEl( galleryItems ) )
+// получим родителя для нашей галереии
+const gallerey = document.querySelector( '.gallery' )
+
+
+// функция динамической разметки передаем в нее любой  массив изображений и рендерим разметку
+const html = (gallery) => {
+    return gallery.map( ( img ) => {
+        const imgEl = new Gallerey1( img.preview, img.original, img.description )
+        const htmlEl = `<a class="gallery__item" href="${imgEl.original}">
+            <img class="gallery__image" src="${imgEl.preview}" title ="${imgEl.description}" alt="${imgEl.description}" />
+            </a>`
+       return htmlEl
+    })
+}
+      
+// повесим нашу разметку на сайт
+gallerey.insertAdjacentHTML("beforeend", html(galleryItems).join(""))
+
 
 // этот кусок кода написан не мной, а взят из документации где в аргументы передаем настройки для новой галереи
 let gallerySet = new SimpleLightbox( '.gallery a', { captionPosition: "bottom", captionDelay: 250 } );
 
 gallerySet.on( 'show.simplelightbox', function () {
     
-	console.log("Получаеться прослушивать клики даже ненужно, все уже есть под капотом")
+    console.log( "Получаеться прослушивать клики даже ненужно, все уже есть под капотом" )
+    
 } );
+
+
+// **********************************предыдущее решение******************************************
+
+
+// const gallerey = document.querySelector( '.gallery' )
+
+// // уже знакомый метод верстки через шаблонную функцию
+// const imgEl = (galleryItems) => {
+//     return galleryItems.map( ( img ) => {
+//         let linkEl = document.createElement( "a" )
+//         linkEl.classList.add( "gallery__item" )
+//         linkEl.href = img.original
+//         let imgEl = document.createElement( "img" )
+//         imgEl.src = img.preview
+//         imgEl.alt = img.description
+//         imgEl.title= img.description
+//         imgEl.classList.add( "gallery__image" )
+//         linkEl.append(imgEl)
+//         return linkEl
+//     })
+// }
+
+// gallerey.append( ...imgEl( galleryItems ) )
+
+// // этот кусок кода написан не мной, а взят из документации где в аргументы передаем настройки для новой галереи
+// let gallerySet = new SimpleLightbox( '.gallery a', { captionPosition: "bottom", captionDelay: 250 } );
+
+// gallerySet.on( 'show.simplelightbox', function () {
+    
+//     console.log( "Получаеться прослушивать клики даже ненужно, все уже есть под капотом" )
+    
+// } );
 
